71272f4bfce5316e70a6162db7cacfffd1318526
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-30 15:47:42 -0700

    pass post POST test

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -1,34 +1,49 @@
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
-import request from 'superagent';
+import request from 'supertest';
 import app from '../lib/app.js';
 import UserService from '../lib/services/UserService.js';
 
-const agent = request.agent(app);
+
 
 describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+
+  let user = {}; 
+  let agent;
+
+  beforeEach(async() => {
+    await setup(pool);
+    agent = await request.agent(app);
+    user = await UserService.create({
+      username: 'clem',
+      password: 'password',
+      profilePhotoUrl: 'picture'
+    });
+    await agent.post('/api/v1/auth/login')
+      .send({
+        username: 'clem',
+        password:'password'
+      });
+
   });
 
-  it('creates a new post via POST', async() => {
-    const res = await request(app)
+  it('creates a post via POST', async() => {
+
+    const res = await agent
       .post('/api/v1/posts')
       .send({
-        user: 'clem',
+        userId: user.id,
         photoUrl: 'picture.jpg',
-        caption: 'check it out everybody',
-        tags: ['broccoli', 'rice', 'passion']
+        caption: 'hey everybody on the website',
+        tags: ['broccoli', 'passion']
       });
 
     expect(res.body).toEqual({
-      user: 'clem',
+      id: '1',
+      userId: user.id,
       photoUrl: 'picture.jpg',
-      caption: 'check it out everybody',
-      tags: ['broccoli', 'rice', 'passion']
+      caption: 'hey everybody on the website',
+      tags: ['broccoli', 'passion']
     });
   });
-
-  
 });
-
