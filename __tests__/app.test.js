4e8b27c95de906953e3dbc07341caa175c3dfdaf
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-30 10:59:36 -0700

    fix git password issue, install bcrypt, create user model and  pass POST

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,22 +3,25 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+
+
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-
-  it('sings a user up via POST', async() => {
+  it('signs up a user via POST', async() => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
       .send({
-        email: 'green@jogging.com',
-        password: 'parklover94'
+        email: 'test@test.com',
+        password: 'password',
+        profilePhotoUrl: expect.any(String)
       });
 
     expect(res.body).toEqual({
       id: '1',
-      email: 'green@jogging.com'
+      email: 'test@test.com',
+      profilePhotoUrl: expect.any(String)
     });
   });
 });
---
+++ sql/setup.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users cascade;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    profile_photo_url TEXT NOT NULL
+);
