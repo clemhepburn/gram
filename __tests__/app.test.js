3a3c01b47bb96a5fd2a81c571e136386ab942c1f
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-30 10:17:26 -0700

    write failing POST signup test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,18 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('sings a user up via POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'green@jogging.com',
+        password: 'parklover94'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'green@jogging.com'
+    });
+  });
 });
