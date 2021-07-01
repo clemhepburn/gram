2fd8a6a3d59cf6f1885c1012a6ef13db833b2e2e
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-07-01 10:23:10 -0700

    create failing comment delete test

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -4,6 +4,7 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import UserService from '../lib/services/UserService.js';
 import Post from '../lib/models/Post.js';
+import Comment from '../lib/models/Comment.js';
 
 describe('demo routes', () => {
 
@@ -35,7 +36,7 @@ describe('demo routes', () => {
       caption: 'Here i am again',
       tags: ['unending riddle', 'squelching', 'exponent']
     });
-    
+
     const res = await agent
       .post('/api/v1/comments')
       .send({
@@ -50,6 +51,22 @@ describe('demo routes', () => {
       post: post.id,
       comment: 'Pretty neat!'
     });
+  });
+
+  it('deletes a comment via DELETE', async() => {
+    const comment = await Comment.insert({
+      id: '1',
+      commentBy: user.id,
+      post: post.id,
+      comment: 'Want 500 dollars today? follow for free giveaway'
+    });
+    
+    const res = await agent
+      .delete(`/api/v1/comments/${comment.id}`)
+      .send(comment);
+
+    expect(res.body).toEqual(comment);
+
   });
 
 });
